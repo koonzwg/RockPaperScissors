b4aa7fee45ce0fc4569ce80cf6b874b1d9f5c13f
koonzwg <koonzwg@gmail.com>
2024-03-17 12:54:11 -0500

    Day 25 complete

--- RockPaperScissors/ContentView.swift
+++ RockPaperScissors/ContentView.swift
@@ -8,14 +8,73 @@
 import SwiftUI
 
 struct ContentView: View {
+    let moves = ["✊", "✋", "✌️"]
+    @State private var appChoice = Int.random(in: 0...2)
+    @State private var shouldWin = Bool.random()
+    @State private var score = 0
+    @State private var questionCount = 1
+    @State private var showingResult = false
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Score: \(score)")
+                .font(.headline)
+            
+            Text("App's Move: \(moves[appChoice])")
+                .font(.system(size: 20))
+            
+            if shouldWin {
+                Text("You should WIN!")
+                    .font(.headline)
+                    .foregroundColor(.green)
+            } else {
+                Text("You should LOSE!")
+                    .font(.headline)
+                    .foregroundColor(.red)
+            }
+            
+            HStack {
+                ForEach(0..<3) { index in
+                    Button(action: {
+                        self.playerTapped(index)
+                    }) {
+                        Text(self.moves[index])
+                            .font(.system(size: 100))
+                    }
+                }
+            }
         }
-        .padding()
+        .alert(isPresented: $showingResult) {
+            Alert(title: Text("Game Over"), message: Text("Your score is \(score) out of 10."), dismissButton: .default(Text("Play Again")) {
+                self.resetGame()
+            })
+        }
+    }
+    
+    func playerTapped(_ index: Int) {
+        let winningMoves = [1, 2, 0]
+        let didWin = index == winningMoves[appChoice]
+        
+        if (didWin && shouldWin) || (!didWin && !shouldWin) {
+            score += 1
+        } else {
+            score -= 1
+        }
+        
+        if questionCount == 10 {
+            showingResult = true
+        } else {
+            questionCount += 1
+            appChoice = Int.random(in: 0...2)
+            shouldWin.toggle()
+        }
+    }
+    
+    func resetGame() {
+        score = 0
+        questionCount = 1
+        appChoice = Int.random(in: 0...2)
+        shouldWin = Bool.random()
     }
 }
 
